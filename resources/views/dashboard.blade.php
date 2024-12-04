e600f8bc16f3ac04cd4aa6530cd24033e08e1768
Lloyd McKinney <42.lloyd.mckinney@gmail.com>
2024-12-04 11:55:05 +0100

    Experimental 1st question addition

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,16 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    {{ __("You're logged in!") }}  
+                </div>
+                <div>
+                    <form method="POST">
+                        @csrf
+                        <h3>Question 1: What is your favorite brand of cigarettes?</h3>
+                        <input type="radio" name="question1" value="red"> Marlboro<br>
+                        <input type="radio" name="question1" value="blue"> Chesterfield<br>
+                        <input type="radio" name="question1" value="green"> L&M<br>
+                    </form>
                 </div>
             </div>
         </div>
